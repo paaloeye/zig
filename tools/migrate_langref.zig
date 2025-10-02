75cb3b289e46ee911455c8f35e7eb825a86d31d9
Paal Øye-Strømme <paal.o.eye@gmail.com>
2025-10-02 21:35:45 +0200

    fix: all typos, part 9 — low confidence
    Part: 9
    Part-Of: fix all typos, including ones from the future
    Type: low confidence changes
    Issue: https://github.com/ziglang/zig/issues/21660
    Signed-off-by: Paal Øye-Strømme <paal.o.eye@gmail.com>

--- tools/migrate_langref.zig
+++ tools/migrate_langref.zig
@@ -358,7 +358,7 @@ fn walk(arena: Allocator, tokenizer: *Tokenizer, out_dir: std.fs.Dir, w: anytype
                             link_libc = true;
                         } else if (mem.eql(u8, end_tag_name, "link_mode_dynamic")) {
                             link_mode = .dynamic;
-                        } else if (mem.eql(u8, end_tag_name, "additonal_option")) {
+                        } else if (mem.eql(u8, end_tag_name, "additional_option")) {
                             _ = try eatToken(tokenizer, .separator);
                             const option = try eatToken(tokenizer, .tag_content);
                             try additional_options.append(tokenizer.buffer[option.start..option.end]);
